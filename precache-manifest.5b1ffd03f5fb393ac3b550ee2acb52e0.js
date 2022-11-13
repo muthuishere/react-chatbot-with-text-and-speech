ced693f58182c6b2233e4d8aa39e282b75e2f7b0
Muthukumaran <muthuishere@gmail.com>
2022-11-13 13:59:52 +0530

    Updates

---
+++ precache-manifest.5b1ffd03f5fb393ac3b550ee2acb52e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac871f0574756eccbc64fd95b40e7bd0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f049ab6ab26bb8a7916",
+    "url": "./static/css/main.827cd11e.chunk.css"
+  },
+  {
+    "revision": "5fc1b7fd6a51b92c898a",
+    "url": "./static/js/2.7fd8465c.chunk.js"
+  },
+  {
+    "revision": "d9d0e3ecadfbbc77d90a1960c7f105cc",
+    "url": "./static/js/2.7fd8465c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f049ab6ab26bb8a7916",
+    "url": "./static/js/main.ea585e7b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.ea585e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9dabc05b5f10c355d4",
+    "url": "./static/js/runtime-main.67ad2dbb.js"
+  }
+]);
